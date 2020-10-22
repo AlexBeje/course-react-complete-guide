45041c418a5be90fc7bf178d117fe4ecf2ef2bdb
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-10-22 11:18:58 +0200

    📦 NEW: Added style using an external file.

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import "./Person.css";
+
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
-        My name is {props.name} and I am {props.age} years old. My favorite
-        number is {Math.floor(Math.random() * 30)}
+        My name is {props.name} and I am {props.age} years old.
       </p>
+      {/* <p>My favorite number is {Math.floor(Math.random() * 30)}</p> */}
       <p>{props.children}</p>
       <input type="text" onChange={props.change} value={props.name} />
     </div>
