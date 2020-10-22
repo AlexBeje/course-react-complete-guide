caf36dcd51843f56e48c068b427ab4b9fd919f33
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-10-22 08:02:09 +0200

    📦 NEW: Added the state property and the onClick event.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,37 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Alex", age: 27 },
+      { name: "Bia", age: 21 },
+      { name: "Dario", age: 0 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    console.log("Button clicked");
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, this is your first react app!</h1>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          Hobbies: Riding.
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const person = () => {
+const person = (props) => {
   return (
-    <p>I am a person and I am {Math.floor(Math.random() * 30)} years old.</p>
+    <div>
+      <p>
+        My name is {props.name} and I am {props.age} years old. My favorite
+        number is {Math.floor(Math.random() * 30)}
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
